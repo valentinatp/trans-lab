25a8f44153d33a8696e8e1406b955914aae14e9e
valentinatp <valentina.toledoporma@gmail.com>
2017-07-14 09:32:10 -0400

    Creo index pregunta y agrego estilos y funciones

--- js/main.js
+++ js/main.js
@@ -5,4 +5,25 @@ function openNav() {
 function closeNav() {
     document.getElementById("myMenu").style.width = "0";
 }
+/*
+var boton = document.getElementById("sangurucho");
 
+	boton.addEventListener("click", function(){
+		document.getElementById("contenedor").classList.remove("hidden");
+	});
+
+var btn = document.getElementById("close");
+	btn.addEventListener("click", function(){
+		document.getElementById("contenedor").classList.add("hidden");
+	})
+*/
+
+
+/*
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+	$("#btn").click(function(){
+		$(".menu").remove('hidden');
+	});
+});*/
